0c872fb25113a0150c8f522e55c82f559c85dff8
mthavaf <mthavaf@gmail.com>
2020-03-27 02:01:35 +0530

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// export default ({ name }) => <h1>Hello {name}!</h1>;
+export default ({ name }) => <h1>Hello Client!</h1>;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-appppppp-xmdi1g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-appppppp-xmdi1g)
